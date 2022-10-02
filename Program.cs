b8866bc5920200ed3940f00ef87b91bb91f27c81
Владимир Прялов <110190623+SamFuji@users.noreply.github.com>
2022-10-02 16:36:10 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
+            string str = Console.ReadLine();
+            if (str.Length < 5)
+                Console.WriteLine("В строке меньше 5 символов");
+            else if (str.Length >= 5 && str.Length <= 15)
+                Console.WriteLine(str.Length);
+            else if (str.Length > 15)
+                Console.WriteLine("В строке больше 15 символов");
+            Console.ReadLine();
 
+//Проба подсчёта количества символов в отдельной строке
