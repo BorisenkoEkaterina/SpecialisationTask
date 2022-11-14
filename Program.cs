712057471e97afcfe73643fec4933f813538c4ab
SamFuji <samfuji174@mail.ru>
2022-11-14 15:41:58 +0500

    начал другое решение, более универсальное: строка вводится пользователем без указания длинны массива и формируется не поэтапно, а целиком

--- Program.cs
+++ Program.cs
@@ -15,63 +15,74 @@
 // Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray(); 
 //считывает из консоли строку, разделяет её по пробелам, для каждой подстроки вызывает int.Parse и приводит полученную коллекцию к типу массива.
 
-using System;
-namespace Abcd
-{
-class Program
-    {
-        static void Main()
-        {
-            Console.Write("Введите количество строк, которые хотите ввести: ");
-            int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
-            string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Введите строку №{0}:\r\n    ", i + 1);
-                strs[i] = Console.ReadLine(); //Заполняем его
-            }
-            Console.WriteLine("Вы ввели следующие строки:");
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine(strs[i]);
-            }
-            Console.Write("Нажмите [Enter] для выхода...");
-            Console.ReadLine();
-        }
-    }
-}
-//с форума
-
-// string buf;
-//             int n;
-//             Console.WriteLine("Введите кол-во элементов массива (n):");
-//             buf = Console.ReadLine();
-//             n = Convert.ToInt32(buf);
-//             int[] a = new int[n];
- 
-//             for (int x = 0; x < n; x++)
+// using System;
+// namespace Abcd
+// {
+// class Program
+//     {
+//         static void Main()
+//         {
+//             Console.Write("Введите количество строк, которые хотите ввести: ");
+//             int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
+//             string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+//             for (int i = 0; i < n; i++)
 //             {
-//                 Console.WriteLine("Введите {0} -ый элемент", (x+1));
-//                 buf = Console.ReadLine();
-//                 a[x] = Convert.ToInt32(buf);
+//                 Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+//                 strs[i] = Console.ReadLine(); //Заполняем его
 //             }
+//             Console.WriteLine("Вы ввели следующие строки:");
+//             for (int i = 0; i < n; i++)
+//             {
+//                 Console.WriteLine(strs[i]);
+//             }
+//             Console.Write("Нажмите [Enter] для выхода...");
+//             Console.ReadLine();
+//         }
+//     }
+// }
+// //с форума
+
+// // string buf;
+// //             int n;
+// //             Console.WriteLine("Введите кол-во элементов массива (n):");
+// //             buf = Console.ReadLine();
+// //             n = Convert.ToInt32(buf);
+// //             int[] a = new int[n];
+ 
+// //             for (int x = 0; x < n; x++)
+// //             {
+// //                 Console.WriteLine("Введите {0} -ый элемент", (x+1));
+// //                 buf = Console.ReadLine();
+// //                 a[x] = Convert.ToInt32(buf);
+// //             }
 
 
 
 
-// Возвращает число знаков в текущем объекте String.
-public int Length { get; }
+// // Возвращает число знаков в текущем объекте String.
+// public int Length { get; }
 
 
-string str = "abcdefg";
-Console.WriteLine("1) The length of '{0}' is {1}", str, str.Length);
-Console.WriteLine("2) The length of '{0}' is {1}", "xyz", "xyz".Length);
+// string str = "abcdefg";
+// Console.WriteLine("1) The length of '{0}' is {1}", str, str.Length);
+// Console.WriteLine("2) The length of '{0}' is {1}", "xyz", "xyz".Length);
 
-int length = str.Length;
-Console.WriteLine("3) The length of '{0}' is {1}", str, length);
+// int length = str.Length;
+// Console.WriteLine("3) The length of '{0}' is {1}", str, length);
 
 // This example displays the following output:
 //    1) The length of 'abcdefg' is 7
 //    2) The length of 'xyz' is 3
 //    3) The length of 'abcdefg' is 7
 
+ Console.Write("Введите желаемую строку для обработки: ");
+string originalString = Console.ReadLine();
+string[] myArray = originalString.Split(' ');
+
+Console.WriteLine();
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i < myArray.Length; i++)
+            Console.Write(myArray[i] + " ");
+
+// for (int i = 0; i < myArray.Length; i++)
+// if (str.Length <= 3)
